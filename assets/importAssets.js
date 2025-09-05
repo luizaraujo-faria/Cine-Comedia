0b79521570ca303093bd033ae4d30c608a84b494
luiz <luiz308araujofarias@gmail.com>
2025-09-04 21:44:05 -0300

    Updates

---
+++ assets/importAssets.js
@@ -0,0 +1,27 @@
+import Facebook from './icons/facebook.png';
+import Github from './icons/github.png';
+import Google from './icons/google.png';
+import Instagram from './icons/instagram.png';
+import Twitter from './icons/twitter.png';
+import Whatsapp from './icons/whatsapp.png';
+import Youtube from './icons/youtube.png';
+
+import Banner1 from './images/Banner-2.webp';
+import BannerGG from './images/Banner-GG.webp';
+import BannerAV from './images/Banner-AV.webp';
+
+export const icons = {
+    Facebook,
+    Github,
+    Google,
+    Instagram,
+    Twitter,
+    Whatsapp,
+    Youtube
+}
+
+export const images = {
+    Banner1,
+    BannerGG,
+    BannerAV
+}
